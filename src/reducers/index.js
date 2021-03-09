fc222f17148240047e0cb2c40222f9577a93a70d
MintoJ <jeff_alan@hotmail.com>
2021-03-09 11:52:33 +0000

    Adds RentalDetail and RentalListing Page. Adds bootstrap and scss. Adds Redux and Routing.

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import * as redux from 'redux';
+import thunk from 'redux-thunk';
+import { rentalReducer, selectedRentalReducer } from './rental-reducer';
+
+
+export const init = () => {
+    const reducer = redux.combineReducers({
+        rentals: rentalReducer,
+        rental : selectedRentalReducer
+    });
+    
+    const store = redux.createStore(reducer, redux.applyMiddleware(thunk));
+    return store;
+}
